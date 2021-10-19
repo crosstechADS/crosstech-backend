bb4eaa2247042f99908e2903f1293b2dfe996d13
robarsotti <54046460+robarsotti@users.noreply.github.com>
2021-10-19 13:37:47 -0300

    alteração timeout

--- index.js
+++ index.js
@@ -8,9 +8,9 @@ require("dotenv-safe").config();
 
 const db = mysql.createPool({
     connectionLimit: 1000,
-    connectTimeout: 60 * 60 * 1000,
-    acquireTimeout: 60 * 60 * 1000,
-    timeout: 60 * 60 * 1000,
+    connectTimeout: 60 * 60 * 1000 * 60,
+    acquireTimeout: 60 * 60 * 1000 * 60,
+    timeout: 60 * 60 * 1000 * 60,
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
