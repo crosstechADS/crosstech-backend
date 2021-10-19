0127612ab5eb2eb48da94b23c5601c88ba6d8179
robarsotti <54046460+robarsotti@users.noreply.github.com>
2021-10-19 12:56:00 -0300

    adição tempo timeout

--- index.js
+++ index.js
@@ -7,7 +7,10 @@ const saltRounds = 10;
 require("dotenv-safe").config();
 
 const db = mysql.createPool({
-
+    connectionLimit: 1000,
+    connectTimeout: 60 * 60 * 1000,
+    acquireTimeout: 60 * 60 * 1000,
+    timeout: 60 * 60 * 1000,
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
