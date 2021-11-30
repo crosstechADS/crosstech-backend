f78c9d4e675a4b0b740f7b2bb9eafa6c861e56ec
Guilherme Brito <hiraygui@gmail.com>
2021-11-30 13:36:08 -0300

    adicionando função para redirecionar http para https

--- index.js
+++ index.js
@@ -25,11 +25,11 @@ const db = mysql.createPool({
 app.use(cors());
 app.use(express.json());
 
-app.get('/', function(req, res, next) {
-    if (req.protocol == 'http') {
-        res.redirect('https://' + 
-        req.get('host') + req.originalUrl);
-    }
+app.use((req, res, next) => { //Cria um middleware onde todas as requests passam por ele
+    if ((req.headers["x-forwarded-proto"] || "").endsWith("http")) //Checa se o protocolo informado nos headers é HTTP
+        res.redirect(`https://${req.hostname}${req.url}`); //Redireciona pra HTTPS
+    else //Se a requisição já é HTTPS
+        next(); //Não precisa redirecionar, passa para os próximos middlewares que servirão com o conteúdo desejado
 });
 
 //Nessa função estamos criando a verificação do token recebido.
