6d33295d82ed84296f4369caa07cac671dc8deff
urdiali <vitorurdiali@gmail.com>
2021-11-30 14:16:57 -0300

    Retirando a tentativa de redirecionamento do http

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-app.use((req, res, next) => { //Cria um middleware onde todas as requests passam por ele
+/*app.use((req, res, next) => { //Cria um middleware onde todas as requests passam por ele
     if ((req.headers["x-forwarded-proto"] || "").endsWith("http")) //Checa se o protocolo informado nos headers é HTTP
         res.redirect(`https://${req.hostname}${req.url}`); //Redireciona pra HTTPS
     else //Se a requisição já é HTTPS
         next(); //Não precisa redirecionar, passa para os próximos middlewares que servirão com o conteúdo desejado
-});
+});*/
 
 const express = require('express');
 const cors = require('cors');
