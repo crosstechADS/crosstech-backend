7ea83a52386d64750effe4ac0962f21302806bef
robarsotti <54046460+robarsotti@users.noreply.github.com>
2022-04-27 01:26:46 -0300

    reestruturação login controller

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ app.use(express.json());
 // @controllers
 const { treinoRegister, treinoSelect } = require('./src/controllers/treino.controller')
 const { usuariosRegister } = require('./src/controllers/usuarios.controller')
+const { login } = require('./src/controllers/login.controller')
 
 //Nessa função estamos criando a verificação do token recebido.
 function verifyJWT(req, res, next) {
@@ -160,55 +161,7 @@ app.post("/calculoIMC", (req, res) => {
     res.json({ IMC: imc });
 })
 
-app.post("/login", (req, res) => {
-    const email = req.body.email;
-    const password = req.body.password;
-    var id;
-    var tipoPerfil;
-    db.query("SELECT usu.ID_USUARIO, perf.DS_TIPO_PERFIL FROM tb_usuarios usu INNER JOIN tb_grupos_usuarios gru ON gru.ID_USUARIO = usu.ID_USUARIO INNER JOIN heroku_56d9955fbec8988.tb_tipo_perfil perf on gru.ID_TIPO_PERFIL = perf.ID_TIPO_PERFIL WHERE usu.DS_EMAIL = ?", [email],
-        (err, result) => {
-            if (err) {
-                res.send(err)
-            }
-            if (result?.length) {
-                id = result[0].ID_USUARIO
-                tipoPerfil = result[0].DS_TIPO_PERFIL
-            }
-        }
-    )
-
-    db.query("SELECT * FROM TB_USUARIOS WHERE DS_EMAIL = ?", [email],
-        (err, result) => {
-            console.log(result)
-            if (err) {
-                res.send(err)
-            }
-            if (result?.length) {
-                bcrypt.compare(password, result[0].DS_SENHA,
-                    (err, result) => {
-                        if (result) {
-                            //criação do JWT -
-                            //Primeiro parâmetro passo o ID do cliente para geração do token.
-                            //Segundo parâmetro passo o SECRET, código do servidor para criptografar e descriptografar.
-                            //Terceiro parâmetro é referente ao tempo de expiração do token.
-                            const token = jwt.sign({ id }, process.env.SECRET, { expiresIn: 3000 })
-                            res.send({ msg: "Usuário logado com sucesso!", auth: true, token: token, perfil: tipoPerfil, Email: email })
-                        }
-                        else {
-                            res.status(401)
-                            res.send({ msg: "Senha incorreta." })
-                        }
-
-                    });
-
-            }
-            else {
-                //res.status(404)
-                res.send({ msg: "Usuário não encontrado." })
-            }
-        }
-    );
-});
+app.post("/login", login);
 
 app.post("/exerciciosregister", (req, res) => {
     const exercicio = req.body.exercicio;
---
+++ src/controllers/login.controller.js
@@ -0,0 +1,56 @@
+const { db } = require('../config/database')
+const bcrypt = require("bcrypt");
+const jwt = require('jsonwebtoken');
+require("dotenv-safe").config();
+
+const login = (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+    var id;
+    var tipoPerfil;
+    db.query("SELECT usu.ID_USUARIO, perf.DS_TIPO_PERFIL FROM tb_usuarios usu INNER JOIN tb_grupos_usuarios gru ON gru.ID_USUARIO = usu.ID_USUARIO INNER JOIN heroku_56d9955fbec8988.tb_tipo_perfil perf on gru.ID_TIPO_PERFIL = perf.ID_TIPO_PERFIL WHERE usu.DS_EMAIL = ?", [email],
+        (err, result) => {
+            if (err) {
+                res.send(err)
+            }
+            if (result?.length) {
+                id = result[0].ID_USUARIO
+                tipoPerfil = result[0].DS_TIPO_PERFIL
+            }
+        }
+    )
+
+    db.query("SELECT * FROM TB_USUARIOS WHERE DS_EMAIL = ?", [email],
+        (err, result) => {
+            console.log(result)
+            if (err) {
+                res.send(err)
+            }
+            if (result?.length) {
+                bcrypt.compare(password, result[0].DS_SENHA,
+                    (err, result) => {
+                        if (result) {
+                            //criação do JWT -
+                            //Primeiro parâmetro passo o ID do cliente para geração do token.
+                            //Segundo parâmetro passo o SECRET, código do servidor para criptografar e descriptografar.
+                            //Terceiro parâmetro é referente ao tempo de expiração do token.
+                            const token = jwt.sign({ id }, process.env.SECRET, { expiresIn: 3000 })
+                            res.send({ msg: "Usuário logado com sucesso!", auth: true, token: token, perfil: tipoPerfil, Email: email })
+                        }
+                        else {
+                            res.status(401)
+                            res.send({ msg: "Senha incorreta." })
+                        }
+
+                    });
+
+            }
+            else {
+                //res.status(404)
+                res.send({ msg: "Usuário não encontrado." })
+            }
+        }
+    );
+};
+
+module.exports = { login }
